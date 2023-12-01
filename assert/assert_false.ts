67013e703d6d1e5e5b6365c7392814ff5164ab9d
Asher Gomez <ashersaupingomez@gmail.com>
2023-12-01 11:29:21 +1100

    chore(assert): complete documentation

--- assert/assert_false.ts
+++ assert/assert_false.ts
@@ -1,7 +1,8 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 import { AssertionError } from "./assertion_error.ts";
 
-type Falsy = false | 0 | 0n | "" | null | undefined;
+/**  */
+export type Falsy = false | 0 | 0n | "" | null | undefined;
 
 /** Make an assertion, error will be thrown if `expr` have truthy value. */
 export function assertFalse(expr: unknown, msg = ""): asserts expr is Falsy {
--- assert/assertion_error.ts
+++ assert/assertion_error.ts
@@ -1,7 +1,19 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+
+/** Error thrown when an assertion fails. */
 export class AssertionError extends Error {
-  override name = "AssertionError";
+  /**
+   * Constructs a new {@linkcode AssertionError} instance.
+   *
+   * @example
+   * ```
+   * import { AssertionError } from "https://deno.land/std@$STD_VERSION/assert/assertion_error.ts";
+   *
+   * throw new AssertionError("foo is missing");
+   * ```
+   */
   constructor(message: string) {
     super(message);
+    this.name = this.constructor.name;
   }
 }
